17acc1777082f8440126e9be1812f880de35b6ca
Jarno <98905852+ArcturusPhoenix@users.noreply.github.com>
2024-01-17 11:04:39 +0100

    Add files via upload
    emf_source added.

---
+++ emf_source/Unit1.pas
@@ -0,0 +1,149 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics,
+  Controls, Forms, Dialogs, StdCtrls, ExtCtrls, Math;
+
+type
+  TComplex = record
+    Re, Im: Double;
+  end;
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    GroupBox1: TGroupBox;
+    Panel1: TPanel;
+    PaintBox1: TPaintBox;
+    ComboBox1: TComboBox;
+    procedure Button1Click(Sender: TObject);
+    procedure PaintBox1Paint(Sender: TObject);
+  private
+    MaxIterations: Integer;
+    Scale: Double;
+    MandelbrotBitmap: TBitmap;
+    procedure CalculateMandelbrot;
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Panel1.Color := clRed;
+  Panel1.Caption := 'Processing...';
+
+  PaintBox1.Repaint;
+  CalculateMandelbrot;
+end;
+
+procedure TForm1.PaintBox1Paint(Sender: TObject);
+begin
+  if Assigned(MandelbrotBitmap) then
+    PaintBox1.Canvas.StretchDraw(PaintBox1.ClientRect, MandelbrotBitmap);
+end;
+
+procedure TForm1.CalculateMandelbrot;
+var
+  X, Y: Integer;
+  C, Z, ZTemp: TComplex;
+  Iterations: Integer;
+  ScaleValue: Double;
+  MaxCount: Integer;
+  IterationCount: array of array of Integer;
+  ColorValue, ColorDistance: Integer;
+  R, G, B: Byte;
+begin
+  MandelbrotBitmap := TBitmap.Create;
+  MandelbrotBitmap.Canvas.Brush.Color := clBlack;
+  MandelbrotBitmap.Width := PaintBox1.Width;
+  MandelbrotBitmap.Height := PaintBox1.Height;
+
+  ScaleValue := 0.002;
+  MaxCount := StrToIntDef(Edit1.Text, 128);
+  //MaxCount := 128;
+
+  SetLength(IterationCount, MandelbrotBitmap.Width, MandelbrotBitmap.Height);
+
+  for Y := 0 to MandelbrotBitmap.Height - 1 do
+  begin
+    for X := 0 to MandelbrotBitmap.Width - 1 do
+    begin
+      C.Re := (X - PaintBox1.Width / 2) * ScaleValue;
+      C.Im := (Y - PaintBox1.Height / 2) * ScaleValue;
+
+      Z.Re := 0;
+      Z.Im := 0;
+
+      Iterations := MaxCount;
+
+      repeat
+        ZTemp.Re := Z.Re * Z.Re - Z.Im * Z.Im + C.Re;
+        ZTemp.Im := 2 * Z.Re * Z.Im + C.Im;
+
+        Z := ZTemp;
+        Dec(Iterations);
+      until (Sqr(Z.Re) + Sqr(Z.Im) > 4) or (Iterations = 0);
+
+      IterationCount[X, Y] := Iterations;
+    end;
+  end;
+
+  for Y := 0 to MandelbrotBitmap.Height - 1 do
+  begin
+    for X := 0 to MandelbrotBitmap.Width - 1 do
+    begin
+      if IterationCount[X, Y] = MaxCount then
+      begin
+        R := 0;
+        G := 0;
+        B := 0;
+      end
+      else
+      begin
+        ColorValue := Trunc((IterationCount[X, Y] / MaxCount) * 255);
+
+        if IterationCount[X, Y] > MaxCount div 2 then
+        begin
+          ColorDistance := MaxCount - IterationCount[X, Y];
+          B := 0;
+          G := ColorDistance * 255 div (MaxCount div 2);
+          R := G;
+        end
+        else
+        begin
+          ColorDistance := MaxCount div 2 - IterationCount[X, Y];
+          B := ColorDistance * 255 div (MaxCount div 2);
+          G := B;
+          R := 255;
+        end;
+      end;
+
+      MandelbrotBitmap.Canvas.Pixels[X, Y] := RGB(R, G, B);
+    end;
+
+    if Y mod 32 = 0 then
+    begin
+      PaintBox1.Canvas.StretchDraw(PaintBox1.ClientRect, MandelbrotBitmap);
+      Application.ProcessMessages;
+    end;
+  end;
+
+  PaintBox1.Canvas.StretchDraw(PaintBox1.ClientRect, MandelbrotBitmap);
+  Edit3.Text := IntToStr(MaxIterations);
+  Panel1.Color := clGreen;
+  Panel1.Caption := 'Finished';
+end;
+
+end.
+
---
+++ emf_source/Unit1.~dfm
@@ -0,0 +1,93 @@
+object Form1: TForm1
+  Left = 192
+  Top = 125
+  Width = 512
+  Height = 512
+  Caption = 'Form1'
+  Color = clBlack
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  WindowState = wsMaximized
+  PixelsPerInch = 96
+  TextHeight = 13
+  object PaintBox1: TPaintBox
+    Left = 0
+    Top = 89
+    Width = 496
+    Height = 384
+    Align = alClient
+  end
+  object GroupBox1: TGroupBox
+    Left = 0
+    Top = 0
+    Width = 496
+    Height = 89
+    Align = alTop
+    TabOrder = 0
+    object Edit1: TEdit
+      Left = 264
+      Top = 16
+      Width = 121
+      Height = 21
+      TabOrder = 0
+      Text = '256'
+    end
+    object Edit2: TEdit
+      Left = 264
+      Top = 40
+      Width = 121
+      Height = 21
+      TabOrder = 1
+      Text = '0.002'
+    end
+    object Button1: TButton
+      Left = 8
+      Top = 16
+      Width = 121
+      Height = 33
+      Caption = 'Zeichnen'
+      TabOrder = 2
+      OnClick = Button1Click
+    end
+    object Edit3: TEdit
+      Left = 264
+      Top = 64
+      Width = 121
+      Height = 21
+      TabOrder = 3
+    end
+    object Panel1: TPanel
+      Left = 136
+      Top = 51
+      Width = 121
+      Height = 33
+      Caption = 'Idle'
+      Color = clYellow
+      TabOrder = 4
+    end
+    object ComboBox1: TComboBox
+      Left = 8
+      Top = 56
+      Width = 121
+      Height = 21
+      ItemHeight = 13
+      TabOrder = 5
+      Text = '8'
+      Items.Strings = (
+        '8'
+        '16'
+        '24'
+        '32'
+        '64'
+        '128'
+        '256'
+        '512'
+        '1024'
+        '2048')
+    end
+  end
+end
